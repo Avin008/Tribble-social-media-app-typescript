986dd2399585081a02eb6ae3ea30da7050981323
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-08-12 19:32:34 +0530

    feat: added logged-in-user and suggestions component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
+import LoggedInUserCard from "./logged-in-user-card/LoggedInUserCard";
 import Navbar from "./navbar/Navbar";
+import Suggestions from "./suggestions/Suggestions";
 import VerticalPostCard from "./vertical-post-card/VerticalPostCard";
 
-export {Navbar, VerticalPostCard}
+
+export {Navbar, VerticalPostCard, LoggedInUserCard, Suggestions}
