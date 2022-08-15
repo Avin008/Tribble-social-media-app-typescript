0a761d0e2183deb165df2ad88abd0f60a2b36a49
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-08-16 00:41:16 +0530

    feat: added profile-page user functionality

--- src/components/user-profile-card/UserProfileCard.jsx
+++ src/components/user-profile-card/UserProfileCard.jsx
@@ -1,12 +1,25 @@
+import { useSelector } from "react-redux";
+import { useQuery } from "@tanstack/react-query";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../firebase/firebaseConfig";
 const UserProfileCard = () => {
+  const { token } = useSelector((store) => store.authSlice);
+
+  const { data, isLoading } = useQuery(["users"], async () => {
+    const userDocRef = doc(db, "users", token);
+    return (await getDoc(userDocRef)).data();
+  });
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div className="flex h-fit w-full items-center gap-12 rounded-lg border border-gray-500 bg-white p-4">
       <div className="">
         <div className="h-32 w-32">
           <img
-            src={
-              "https://images.unsplash.com/photo-1570015652016-f4d63e51b2c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTM0fHxwcm9maWxlJTIwcGljdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
-            }
+            src={data.profileImg}
             alt=""
             className="h-full w-full rounded-full object-cover"
           />
@@ -15,7 +28,7 @@ const UserProfileCard = () => {
 
       <div className="flex flex-col flex-wrap space-y-4">
         <div className="flex items-center justify-between">
-          <h4 className="text-3xl font-light">{"Natasha Bora"}</h4>
+          <h4 className="text-3xl font-light">{data.username}</h4>
 
           <button className="cursor-pointer rounded-md border-0 bg-purple-700 px-4 py-1 text-sm font-medium text-white">
             Follow
@@ -23,25 +36,25 @@ const UserProfileCard = () => {
         </div>
         <div className="flex space-x-8">
           <h5 className="font-medium">
-            <span className="font-semibold">{10}</span> Posts
+            <span className="font-semibold">{data.posts.length}</span> Posts
           </h5>
           <h5 className="font-medium">
-            <span className="font-semibold">{5}</span> Followers
+            <span className="font-semibold">{data.followers.length}</span>{" "}
+            Followers
           </h5>
           <h5 className="font-medium">
-            <span className="font-semibold">{20}</span> Following
+            <span className="font-semibold">{data.following.length}</span>{" "}
+            Following
           </h5>
         </div>
         <div className="space-y-1">
-          <h5 className="font-semibold">{"Natasha Bora"}</h5>
-          <h5 className="text-base font-medium">
-            {"Web Developer 😎 | Javascript ❤"}
-          </h5>
+          <h5 className="font-semibold">{data.fullname}</h5>
+          <h5 className="text-base font-medium">{data.bio}</h5>
           <a
-            href="www.google.com"
+            href={`https://www.{data.portfolio}`}
             className="text-none font-medium text-purple-800"
           >
-            natashabora.netlify.app
+            {data.portfolio}
           </a>
         </div>
       </div>
