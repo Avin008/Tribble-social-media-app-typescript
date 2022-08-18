4a748f55f36a4a265bf0c131c641a2e9ef13228a
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-08-18 18:26:26 +0530

    feat: added edit profile link

--- src/components/user-profile-card/UserProfileCard.jsx
+++ src/components/user-profile-card/UserProfileCard.jsx
@@ -1,8 +1,10 @@
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const UserProfileCard = ({ data }) => {
   const { userPostsData, userData } = data;
   const { token } = useSelector((store) => store.authSlice);
+  const navigate = useNavigate();
 
   return (
     <div className="flex h-fit w-full items-center gap-12 rounded-lg border border-gray-500 bg-white p-4">
@@ -21,7 +23,10 @@ const UserProfileCard = ({ data }) => {
           <h4 className="text-3xl font-light">{userData.username}</h4>
 
           {userData.userId === token ? (
-            <button className="cursor-pointer rounded-md border-0 bg-purple-700 px-4 py-1 text-sm font-medium text-white">
+            <button
+              className="cursor-pointer rounded-md border-0 bg-purple-700 px-4 py-1 text-sm font-medium text-white"
+              onClick={() => navigate(`/edit-profile/${userData.userId}`)}
+            >
               Edit Profile
             </button>
           ) : (
