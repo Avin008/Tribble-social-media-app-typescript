1c01b17efad0c49d5ad7fc0ff923ab1aab715629
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-08-20 19:02:55 +0530

    feat: added explore-page

--- src/components/explore-card/ExploreCard.jsx
+++ src/components/explore-card/ExploreCard.jsx
@@ -1,7 +1,15 @@
+import { useDispatch } from "react-redux";
+import { openPostModal } from "../../redux-toolkit/features/postModalSlice";
+
 const ExploreCard = ({ data }) => {
+  const dispatch = useDispatch();
+
   return (
-    <div className="h-full w-full cursor-pointer">
-      <img className="h-full w-full object-cover " src={data.img} alt="" />
+    <div
+      className="h-full w-full cursor-pointer transition hover:bg-black/100"
+      onClick={() => dispatch(openPostModal({ postID: data.postID }))}
+    >
+      <img className="h-full w-full object-cover " src={data?.img} alt="" />
     </div>
   );
 };
