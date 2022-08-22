10e07461eeaf42de3039f808ac8dd332f33d73d8
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-08-22 13:59:36 +0530

    update: guest login info

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   };
 
   const guestLogin = () => {
-    setLoginInfo({ email: "maxdoe@gmail.com", password: "123456" });
+    setLoginInfo({ email: "maricaward@gmail.com", password: "123456" });
   };
 
   const signinUser = async ({ email, password }) => {
